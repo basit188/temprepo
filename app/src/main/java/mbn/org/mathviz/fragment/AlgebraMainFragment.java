f5da28cb8473e3e46d6a061cf22af1f8196cff8c
basit188 <basit188@gmail.com>
2018-02-03 11:27:03 +0530

    SecondCommit

--- app/src/main/java/mbn/org/mathviz/fragment/AlgebraMainFragment.java
+++ app/src/main/java/mbn/org/mathviz/fragment/AlgebraMainFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import mbn.org.mathviz.R;
 
@@ -16,21 +17,28 @@ import mbn.org.mathviz.R;
  */
 
 public class AlgebraMainFragment extends Fragment implements View.OnClickListener{
+    FragmentTransaction fragmentTransaction;
+    AlgebraFragment algebraFragment;
+    FragmentManager fragmentManager;
+    CardView cardAlgebra;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_algebra_main,container,false);
         return v;
     }
 
     public void onViewCreated(View v, Bundle b){
-        CardView cardAlgebra = v.findViewById(R.id.card_algebra);
+        cardAlgebra = v.findViewById(R.id.card_algebra);
         cardAlgebra.setOnClickListener(this);
     }
 
     public void onClick(View v) {
-        AlgebraFragment algebraFragment = new AlgebraFragment();
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        cardAlgebra.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        algebraFragment = new AlgebraFragment();
+        fragmentManager = getActivity().getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,android.R.anim.fade_out,android.R.anim.fade_in,android.R.anim.fade_out);
         fragmentTransaction.replace(R.id.algebraContainer,algebraFragment).addToBackStack("hh'").commit();
     }
+
 }
--- app/src/main/java/mbn/org/mathviz/fragment/ContentsFragment.java
+++ app/src/main/java/mbn/org/mathviz/fragment/ContentsFragment.java
@@ -20,6 +20,8 @@ public class ContentsFragment extends Fragment {
 
     FrameLayout algebraContainer, geometryContainer;
     Fragment algebraMain, geometryMain;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_contents,container,false);
@@ -27,25 +29,19 @@ public class ContentsFragment extends Fragment {
     }
 
     public void onViewCreated(View v, Bundle b){
-//        cardAlgebra = v.findViewById(R.id.card_algebra);
-//        cardGeometry = v.findViewById(R.id.card_geometry);
         algebraContainer = v.findViewById(R.id.algebraContainer);
         geometryContainer = v.findViewById(R.id.geometryContainer);
         algebraMain = new AlgebraMainFragment();
         geometryMain = new GeometryMainFragment();
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentManager = getActivity().getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.algebraContainer,algebraMain);
         fragmentTransaction.add(R.id.geometryContainer,geometryMain);
         fragmentTransaction.commit();
-        //        cardAlgebra.setOnClickListener(this);
     }
 
-    public void onClick(View v) {
-        AlgebraFragment algebraFragment = new AlgebraFragment();
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,android.R.anim.fade_out,android.R.anim.fade_in,android.R.anim.fade_out);
-        fragmentTransaction.replace(R.id.algebraContainer,algebraFragment).addToBackStack("hh'").commit();
+    public void onResume() {
+        super.onResume();
     }
+
 }
